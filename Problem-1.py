82094d0dc78e31b9b97d418b4e94637eafc0a596
Amar Singh <147243156+amarsin22@users.noreply.github.com>
2025-11-28 22:54:43 +0530

    Update Problem-1.py

--- Problem-1.py
+++ Problem-1.py
@@ -1,1 +1,18 @@
 
+class Calculator:
+    def calculate(self, a, b, op):
+        if op == "add":
+            return a + b
+        elif op == "sub":
+            return a - b
+        elif op == "mul":
+            return a * b
+        elif op == "div":
+            if b == 0:
+                return "Cannot divide by zero"
+            return a / b
+        else:
+            return "Invalid operation"
+
+calc = Calculator()
+print(calc.calculate(10, 5, "add"))
