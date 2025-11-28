ede5cbd50b91fab1826c06c6fc5d40c2a659b72d
Amar Singh <147243156+amarsin22@users.noreply.github.com>
2025-11-28 22:58:15 +0530

    Update Problem-4.py

--- Problem-4.py
+++ Problem-4.py
@@ -1,1 +1,12 @@
+numbers = [1,2,8,9,12,46,76,82,15,20,30]
+output = {}
+
+for n in range(1, 10):
+    count = 0
+    for x in numbers:
+        if x % n == 0:
+            count += 1
+    output[n] = count
+
+print(output)
 
